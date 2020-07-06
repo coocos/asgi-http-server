7e032086ca271b60f2bebbc376caa1d146343be1
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2020-07-06 15:34:07 +0300

    Make example ASGI application serve HTML

--- asgi/example.py
+++ asgi/example.py
@@ -2,20 +2,27 @@
 
 
 async def app(scope, receive, send):
+    """Example application which servers a simple HTML page"""
+    html = b"""
+    <!doctype html>
+    <html>
+            <head>
+                    <title>Hello ASGI!</title>
+            </head>
+            <body>
+                    <main>
+                    <h1>Hello ASGI!</h1>
+                    </main>
+            </body>
+    </html>
+    """
     await send(
         {
             "type": "http.response.start",
             "status": 200,
-            "headers": [
-                [b"content-type", b"application/json"],
-                [b"content-length", b"44"],
-            ],
+            "headers": [[b"content-type", b"text/html"], [b"content-length", b"269"],],
         }
     )
     await send(
-        {
-            "type": "http.response.body",
-            "body": b'{"first_name":"paul","last_name":"atreides"}',
-            "more_body": False,
-        }
+        {"type": "http.response.body", "body": html, "more_body": False,}
     )
